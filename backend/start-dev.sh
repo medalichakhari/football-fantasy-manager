698f4bf9f4ebb1502b78f69d61c6dd7fb6b79a5d
mohamedali69 <medalichakhari.dev@gmail.com>
2025-07-19 14:47:03 +0100

    feat: add start script for development and implement healthcheck for PostgreSQL service in Docker Compose

---
+++ backend/start-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if npx prisma migrate deploy; then
+  echo "Migrations applied successfully!"
+else
+  echo "Migration failed"
+  if npx prisma db push; then
+    echo "Database schema pushed successfully!"
+  else
+    echo "Failed to initialize database schema"
+    exit 1
+  fi
+fi
+
+npm run dev
